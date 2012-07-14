750374669fed63059886925410458a16ad9c3726
ch3rn0v <b.ace.so.nice@gmail.com>
2012-07-15 00:31:29 +0300

    First prototype reworked

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,7 +9,9 @@ $(function(){
         //Прячем всё содержимое, и последовательно отображаем слова с задержкой 100 
 		output_block.html(content).children().hide()
         output_block.children().each(function(i){
-             $(this).delay(100 * i).fadeIn()
+             //180 мс это задержка между словами, 200 мс это время отображения первого слова из всего текста
+             $(this).delay(200 + (180 * i)).fadeIn(0)
+             $(this).delay(180).fadeOut(0)
         })
 	})
 

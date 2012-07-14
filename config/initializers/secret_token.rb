8601c2ab2ce4b34bb7971293e43e353bb1269a49
ch3rn0v <b.ace.so.nice@gmail.com>
2012-07-14 14:59:30 +0300

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treeder::Application.config.secret_token = '110c6bc8dbc66939b890a4a1fe516dcab2f6813406bf3b73ad6207e7baec529e35ae59ac9d4bcaad6178e9bf84ad083e8a22bedbb8e137be03e01f37e6963e34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Treeder::Application.config.session_store :cookie_store, key: '_Treeder_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Treeder::Application.config.session_store :active_record_store

97d1c1a775faf33b9261e9451a2d54436faddf3f
dozenthoughts <alekseydesk@yandex.ru>
2012-07-14 23:41:02 +0400

    first prototype

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,8 +3,13 @@ $(function(){
 	$("#btn").click(function(){
         //удаляем лишние пробелы из текста, и обромляем каждое слово тэгом, чтобы потом 
         //каждый из них можно было отдельно анимировать со своим timeout
-		var content = $("textarea").remove_spaces().split(" ").add_tags("<span>", "</span>")
-		$("#processing-result").html(content)
+		var content = $("textarea").remove_spaces().split(" ").add_tags("<span class='show_word'>", "</span>")
+        var output_block = $("#processing-result")
+		output_block.html(content)
+        output_block.children().each(function(i){
+            $(this).hide()
+            $(this).delay(100 * i).fadeIn()
+        })
 	})
 
 });

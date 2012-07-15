5219140c32f002e7c72a429942b78a0db6984685
ch3rn0v <b.ace.so.nice@gmail.com>
2012-07-15 23:32:48 +0300

    Added ability to change speed and css improvements

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,18 +3,15 @@ $(function(){
   var usr_inpt = document.getElementById("user-input")        
   rslt.style.visibility = 'hidden'
     
-    /*$(function restart_scene(){
-    var usr_inpt1 = document.getElementById("user-input")
-    var rslt1 = document.getElementById("processing-result")
-    usr_inpt1.style.visibility = 'visible'
-    rslt1.style.visibility = 'hidden'
-  })*/
-    
     $("#btn").click(function(){
         //удаляем лишние пробелы из текста, и обромляем каждое слово тэгом, чтобы потом 
         //каждый из них можно было отдельно анимировать со своим timeout
         var content = $("textarea").remove_spaces().split(" ").add_tags("<span class='show_word'>", "</span>")
         var output_block = $("#processing-result")
+                
+        var speed = document.getElementById("speed").value
+        //alert(document.getElementById("speed").value)
+                
         
         //Прячем всё содержимое, и последовательно отображаем слова с задержкой 100 
         output_block.html(content).children().hide()        
@@ -24,15 +21,10 @@ $(function(){
         
         output_block.children().each(function(i){
              //180 мс это задержка между словами, 200 мс это время отображения первого слова из всего текста
-             $(this).delay(200 + (160 * i)).fadeIn(0)
-             $(this).delay(160).fadeOut(0)
+             $(this).delay(200 + (speed * i)).fadeIn(0)
+             $(this).delay(speed).fadeOut(0)
         })
-                
-        //выполняется еще до строки 18
-        //usr_inpt.style.visibility = 'visible'
-        //rslt.style.visibility = 'hidden'
 
-        //restart_scene();                        
 	})
   
   $("#btn_reset").click(function(){

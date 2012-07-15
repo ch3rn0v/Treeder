86373bf2cb3ccbac7b7122737f74b16b96278bb5
dozenthoughts <alekseydesk@yandex.ru>
2012-07-15 14:06:21 +0400

    little refactoring

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,24 +1,24 @@
 $(function(){
   var rslt = document.getElementById("processing-result")
+  var usr_inpt = document.getElementById("user-input")        
   rslt.style.visibility = 'hidden'
-	
-	/*$(function restart_scene(){
+    
+    /*$(function restart_scene(){
     var usr_inpt1 = document.getElementById("user-input")
     var rslt1 = document.getElementById("processing-result")
     usr_inpt1.style.visibility = 'visible'
     rslt1.style.visibility = 'hidden'
   })*/
-	
-	$("#btn").click(function(){
+    
+    $("#btn").click(function(){
         //удаляем лишние пробелы из текста, и обромляем каждое слово тэгом, чтобы потом 
         //каждый из них можно было отдельно анимировать со своим timeout
-		var content = $("textarea").remove_spaces().split(" ").add_tags("<span class='show_word'>", "</span>")
+        var content = $("textarea").remove_spaces().split(" ").add_tags("<span class='show_word'>", "</span>")
         var output_block = $("#processing-result")
         
         //Прячем всё содержимое, и последовательно отображаем слова с задержкой 100 
-		output_block.html(content).children().hide()        
+        output_block.html(content).children().hide()        
         //Прячем форму ввода текста и показываем блок, где по очереди отображаются слова
-        var usr_inpt = document.getElementById("user-input")        
         usr_inpt.style.visibility = 'hidden'
         rslt.style.visibility = 'visible'
         
@@ -36,8 +36,6 @@ $(function(){
 	})
   
   $("#btn_reset").click(function(){
-    var usr_inpt = document.getElementById("user-input")
-    var rslt = document.getElementById("processing-result")
     usr_inpt.style.visibility = 'visible'
     rslt.style.visibility = 'hidden'
   })

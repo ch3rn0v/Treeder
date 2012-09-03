1d1c2de8d546b112870085311948adeee1254dcc
ch3rn0v <b.ace.so.nice@gmail.com>
2012-09-03 17:22:15 +0400

    Unnecessary migrations and home.js.erb deleted

--- db/migrate/20120717072731_remove_user_id_from_text.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromText < ActiveRecord::Migration
-  def up
-    remove_column :texts, :user_id
-      end
-
-  def down
-    add_column :texts, :user_id, :integer
-  end
-end
--- db/migrate/20120719125426_add_user_id_to_texts.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToTexts < ActiveRecord::Migration
-  def change
-    add_column :texts, :user_id, :integer
-
-  end
-end
